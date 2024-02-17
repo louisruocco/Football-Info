d79f5763122e6132a70643724bf201f6e5cd6b69
Louis Ruocco <louisruocco1@gmail.com>
2024-02-17 15:07:50 +0000

    able to append links to email, body needs work though

--- script.ps1
+++ script.ps1
@@ -1,7 +1,33 @@
 $uri = Invoke-WebRequest "https://www.bbc.co.uk/sport/football/scores-fixtures"
 $res = $uri.ParsedHtml.getElementsByClassName('gs-o-list-ui')
 $links = ($res[0] | ForEach-Object{$_.getElementsByTagName('a')}).href -replace 'about:/', ''
-$test = foreach($link in $links){
-    "https://www.bbc.co.uk/$link"
+$matches = foreach($link in $links){
+    "<li>https://www.bbc.co.uk/$link</li>"
 }
-$test
+
+$username = (Get-Content "$PSScriptRoot\utils\username.txt")
+$password = (Get-Content "$PSScriptRoot\utils\password.txt") | ConvertTo-SecureString -AsPlainText -Force
+# $emailAddress = (Get-Content "$PSScriptRoot\utils\secrets.txt")[2]
+
+
+$body = @"
+<h1>Premier League Matches This Weekend</h1>
+<p>Here are a list of football matches happening today</p>
+<ul>
+    $matches
+</ul>
+"@
+
+$email = @{
+    from = $username
+    to = 'louisruocco1@gmail.com'
+    subject = "Noremier League Matches This Weekend"
+    smtpserver = "smtp.gmail.com"
+    body = $body
+    port = 587
+    credential = New-Object System.Management.Automation.PSCredential -ArgumentList $username, $password
+    usessl = $true
+    verbose = $true
+}
+
+Send-MailMessage @email -BodyAsHtml
