22aa1f50d925bb968a8d6f924385e9bb3b2d2135
Louis Ruocco <louisruocco1@gmail.com>
2024-02-17 12:11:30 +0000

    links now available as usable objects for the email body

--- script.ps1
+++ script.ps1
@@ -1,6 +1,7 @@
 $uri = Invoke-WebRequest "https://www.bbc.co.uk/sport/football/scores-fixtures"
 $res = $uri.ParsedHtml.getElementsByClassName('gs-o-list-ui')
 $links = ($res[0] | ForEach-Object{$_.getElementsByTagName('a')}).href -replace 'about:/', ''
-foreach($link in $links){
-    start-process msedge https://www.bbc.co.uk/$link
+$test = foreach($link in $links){
+    "https://www.bbc.co.uk/$link"
 }
+$test
